c2ee3c1dfb2938b036bef7bb5114f339021636c4
Akkarapon Phikulsri <lacakira@gmail.com>
2022-08-25 13:40:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const figlet = require("figlet");
 const gradient = require("gradient-string");
 const morgan = require('morgan');
 const db = require('./models');
-
+const cors = require('cors')
 const app = express()
-app.use(express.json({ limit: "30mb"} ));
+
+app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({
     limit: "30mb",
     extended: true
 }))
 
 // config for only development
-if(process.env.NODE_ENV === "development"){
+if (process.env.NODE_ENV === "development") {
     // cors it's allow to deal with react for localhost at port {PORT} without any problem
     app.use(cors({
         origin: process.env.CLIENT_URL
@@ -22,23 +23,27 @@ if(process.env.NODE_ENV === "development"){
     app.use(morgan('dev'))
 }
 
-app.get('/', (req, res)=>{
-    res.json({msg:"ok"})
+app.get('/', (req, res) => {
+    res.json({ msg: "ok" })
 }
 )
-const PORT =  process.env.SERVE_PORT || 5000
 
-db.sequelize.sync().then(()=>{
+const usersController = require("./users/users.controller");
+app.use('/users', usersController)
+
+const PORT = process.env.SERVE_PORT || 5000
+
+db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
         runningServe(`SERVE ON PORT ${PORT}`);
     });
 })
 
 const runningServe = async (log) => {
-	console.log(`\n--- ${log} ---`);
+    console.log(`\n--- ${log} ---`);
 
-  const msg = `Akkarapon`
-  figlet(msg, (err, data)=>[
-	  console.log(gradient.pastel.multiline(data))
-  ])
+    const msg = `Akkarapon`
+    figlet(msg, (err, data) => [
+        console.log(gradient.pastel.multiline(data))
+    ])
 };
