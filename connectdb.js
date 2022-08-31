c0cefd1576108d7b4c68e16ba78020afd501a614
Akkarapon Phikulsri <lacakira@gmail.com>
2022-08-31 14:53:37 +0700

    Add files via upload

--- connectdb.js
+++ connectdb.js
@@ -1,10 +1,19 @@
+require("dotenv").config(); // this is important!
 const connect = require('tedious').Connection()
 const config = {
-    server: '',
+    server: process.env.DB_HOST,
     authentication: {
-
+        type: 'default',
+        options: {
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD
+        }
     },
     options: {
-        
+        instanceName: "SQLEXPRESS",
+        database: process.env.DB_DATABASE,
+        rowCollectionOnRequestCompletiion: true
     }
 }
+let conn = connect(config)
+module.exports = conn;
