0da755e4084659e4446a1b9b3d64097eff2047ab
Akkarapon Phikulsri <lacakira@gmail.com>
2022-08-31 16:07:46 +0700

    Add files via upload

--- store.controller.js
+++ store.controller.js
@@ -2,30 +2,44 @@
 const conn = require('./connectdb')
 const request = require('tedious').Request;
 
-let executeQuery = function(sql) {
-    req = new request(sql, (err, rowCount, rows)=>{
+let executeQuery = function (sql) {
+    let result = [];
+    req = new request(sql, (err, rowCount, rows) =>{
         if(err){
             console.log(err);
         }else{
-            console.log(rowCount);
+            var jsonArray = []
+            rows.forEach((columns)=>{
+                var rowObj = {};
+                columns.forEach((column)=>{
+                    rowObj[column.metadata.colName] = column.value
+                })
+                console.log(rowObj)
+                jsonArray.push(rowObj)
+            })
+            // res.json(jsonArray)
+            // console.log(jsonArray)
         }
     })
+
+
+
     conn.execSql(req)
 
-   
+
 }
 
-let getAllProduct = function() {
+let getAllProduct = function () {
     let sql = "SELECT * FROM PRODUCTS;";
 
-    conn.on('connect', function(err) {
-        if(err) {
-          console.log('Error: ', err)
+    conn.on('connect', function (err) {
+        if (err) {
+            console.log('Error: ', err)
         }
         executeQuery(sql)
 
-      });
-      conn.connect();
+    });
+    conn.connect();
 };
 
 
